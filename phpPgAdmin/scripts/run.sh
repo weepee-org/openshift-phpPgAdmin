ef3eae285161af157a5bd96311a44f85696f2d9c
Joeri van Dooren <ure@moreorless.be>
2016-02-15 22:06:08 +0100

    test

--- phpPgAdmin/scripts/run.sh
+++ phpPgAdmin/scripts/run.sh
@@ -3,6 +3,7 @@
 STAMP=$(date)
 
 echo "openshift-pgadmin:x:`id -u`:0:openshift-wordpress:/:/sbin/nologin" >> /etc/passwd
+cp -vrpf /pgadmin/* /app/
 
 echo "[${STAMP}] Starting daemon..."
 # run apache httpd daemon
